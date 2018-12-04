023f14df74718eb5719765933f2219ac8d657005
DanielKwanUkYoo <kwanukyoo0722@gmail.com>
2018-12-04 03:49:06 +0000

    added comments what each lines do

--- download_avatars.js
+++ download_avatars.js
@@ -10,33 +10,32 @@ function getRepoContributors(repoOwner, repoName, cb) {
   var option = {
     url: 'https://api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
     headers: {
-      'User-Agent': 'request',             //added UA to header
-      'Authorization': 'token ' + personal.GITHUB_TOKEN //added token that is saved on personal.js
+      'User-Agent': 'request',                              //added UA to header
+      'Authorization': 'token ' + personal.GITHUB_TOKEN     //added token that is saved on personal.js
     }
   }
 
   request(option, function(err, response, body) {
     cb(err, body);
+    var body = JSON.parse(body);                                     //reading each of the url in data
 
-    var body = JSON.parse(body);           //reading each of the url in data
-    // console.log(body);
-    body.forEach(function(element) {
+    body.forEach(function(element) {                                 //getting url data and login data
       var loginName = element.login;
       var avaURL = element.avatar_url;
-      downloadImageByURL(avaURL, './avatars/' + loginName + '.jpg');
+      downloadImageByURL(avaURL, './avatars/' + loginName + '.jpg'); //saves the image to pathfile
     })
 
   });
 }
 
 
-//cb for checking errors
+//cb for checking errors/run
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
 
 });
 
-function downloadImageByURL (url, pathfile) {     //function for getting images
+function downloadImageByURL (url, pathfile) {                        //function for getting images
   request.get(url)
          .pipe(fs.createWriteStream(pathfile))
 }
