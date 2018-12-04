3bf94a6c9742f2447f9bb42d21ed97ae11d54cb5
DanielKwanUkYoo <kwanukyoo0722@gmail.com>
2018-12-04 01:39:48 +0000

    added UA and created repoContributors function

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,21 @@
 var request = require('request');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
+
+
+function getRepoContributors(repoOwner, repoName, cb) {
+  var url = ('https://api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors')
+  request(url, function(err, response, body) {
+    headers = {
+      'User-Agent': 'request'
+
+    }
+  });
+}
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
+
+
